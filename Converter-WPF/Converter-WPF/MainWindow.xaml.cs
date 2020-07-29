d1364cba01dcfb05c055ce8edd3d336d6bff9462
Lemon-383 <47275432+Lemon-383@users.noreply.github.com>
2020-07-30 00:25:07 +0300

    :cold_sweat:

--- Converter-WPF/Converter-WPF/MainWindow.xaml.cs
+++ Converter-WPF/Converter-WPF/MainWindow.xaml.cs
@@ -15,14 +15,52 @@ using System.Windows.Shapes;
 
 namespace Converter_WPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] defaultCurrencies = new string[]
+            {
+                "USD", "EUR", "UAH"
+            };
+
+            foreach (string crnc in defaultCurrencies)
+                AddCurrency(crnc);
+
+            cbox_srcCrnc.SelectedIndex = 0;
+            cbox_trgCrnc.SelectedIndex = 2;
         }
+
+
+        void AddCurrency(string code)
+        {
+            cbox_srcCrnc.Items.Add(code);
+            cbox_trgCrnc.Items.Add(code);
+        }
+
+
+        private void tbox_srcCrncAmount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tbox_trgCrncAmount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        private void tbox_srcToTrgRate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void tbox_trgToSrcRate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
     }
 }
