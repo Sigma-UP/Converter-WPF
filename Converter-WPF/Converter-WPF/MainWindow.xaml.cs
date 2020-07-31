3f80353ca58e6ca2810f67d0ff0e8b286bf289be
Lemon-383 <47275432+Lemon-383@users.noreply.github.com>
2020-07-31 23:17:35 +0300

    Version 0.1.0

--- Converter-WPF/Converter-WPF/MainWindow.xaml.cs
+++ Converter-WPF/Converter-WPF/MainWindow.xaml.cs
@@ -17,6 +17,12 @@ namespace Converter_WPF
 {
     public partial class MainWindow : Window
     {
+        private double srcAmount;
+        private double trgAmount;
+        private double srcRate;
+        private double trgRate;
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,6 +42,11 @@ namespace Converter_WPF
 
             cbox_srcCrnc.SelectedIndex = 0;
             cbox_trgCrnc.SelectedIndex = 2;
+
+            srcAmount = double.Parse(tbox_srcCrncAmount.Text);
+            trgAmount = double.Parse(tbox_trgCrncAmount.Text);
+            srcRate = double.Parse(tbox_srcRate.Text);
+            trgRate = double.Parse(tbox_trgRate.Text);
         }
 
 
@@ -46,27 +57,56 @@ namespace Converter_WPF
         }
 
 
+        private void ConvertSrcAmount()
+        {
+            if (tbox_trgCrncAmount == null || tbox_srcRate == null)
+                return;
+
+            tbox_trgCrncAmount.Text = string.Format("{0:0.00}", trgAmount = srcAmount * srcRate);
+        }
+
+        private void ConvertTrgAmount()
+        {
+            if (tbox_srcCrncAmount == null || tbox_trgRate == null)
+                return;
+
+            tbox_srcCrncAmount.Text = string.Format("{0:0.00}", srcAmount = trgAmount * trgRate);
+        }
+
+
         private void tbox_srcCrncAmount_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (tbox_trgCrncAmount != null && tbox_srcRate != null)
+                if (double.TryParse(tbox_srcCrncAmount.Text, out srcAmount) & double.TryParse(tbox_srcRate.Text, out srcRate))
+                    if (!tbox_trgCrncAmount.IsFocused)
+                        ConvertSrcAmount();
 
         }
 
         private void tbox_trgCrncAmount_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+            if (tbox_srcCrncAmount != null && tbox_trgRate != null)
+                if (double.TryParse(tbox_trgCrncAmount.Text, out trgAmount) & double.TryParse(tbox_trgRate.Text, out trgRate))
+                    if (!tbox_srcCrncAmount.IsFocused)
+                        ConvertTrgAmount();
         }
 
 
-        private void tbox_srcToTrgRate_TextChanged(object sender, TextChangedEventArgs e)
+        private void tbox_srcRate_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (tbox_trgToSrcRate != null) 
-                tbox_trgToSrcRate.Text = (1.0 / double.Parse(tbox_srcToTrgRate.Text)).ToString();
+            if (tbox_trgRate != null)
+                if (double.TryParse(tbox_srcRate.Text, out srcRate))
+                    if(!tbox_trgRate.IsFocused)
+                        tbox_trgRate.Text = string.Format("{0:0.00##}", 1.0 / srcRate);
         }
 
-        private void tbox_trgToSrcRate_TextChanged(object sender, TextChangedEventArgs e)
+        private void tbox_trgRate_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (tbox_srcToTrgRate != null)
-                tbox_srcToTrgRate.Text = (1.0 * double.Parse(tbox_trgToSrcRate.Text)).ToString();
+            if (tbox_srcRate != null)
+                if (double.TryParse(tbox_trgRate.Text, out trgRate))
+                    if (!tbox_srcRate.IsFocused)
+                        tbox_srcRate.Text = string.Format("{0:0.00##}", 1.0 / trgRate);
         }
 
     }
